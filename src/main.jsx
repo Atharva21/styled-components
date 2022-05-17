fb9390c9e3a62bfdd81d3ea6d68f886c59a27c4a
Atharva21 <atharva.m.mulay@gmail.com>
2022-05-17 09:17:48 +0530

    adds baseurl

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
 				<GlobalStyle />
-				<BrowserRouter>
+				<BrowserRouter basename="/styled-components/">
 					<App />
 				</BrowserRouter>
 			</ThemeProvider>
